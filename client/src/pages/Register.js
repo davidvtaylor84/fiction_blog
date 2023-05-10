1b6190427c06491ff259508aa5a41ec246ae8fa4
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2023-05-10 18:53:59 +0100

    working on registering a user

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    async function handleRegisterSubmit(e){
+        e.preventDefault();
+        await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-Type':'application/json'}
+        })
+    }
+
   return (
-    <form className='login'>
+    <form className='login' onSubmit={handleRegisterSubmit}>
         <h2>Register</h2>
-        <input type="text" placeholder='username'/>
-        <input type="password" placeholder='the code'/>
+        <input type="text" placeholder='username' value={username} onChange={handleUsernameChange}/>
+        <input type="password" placeholder='the code' value={password} onChange={handlePasswordChange}/>
         <button>Submit</button>
     </form>
   )
