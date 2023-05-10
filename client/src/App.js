d42bde83fc6710b9d44b8300f2cee04e03a8e2af
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2023-05-10 15:50:48 +0100

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>the surfeit handle</h1>
+        <nav>
+          <a href="">Login</a>
+          {/* <a href="">Register</a> */}
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <img src="https://variety.com/wp-content/uploads/2017/04/the-void.jpg?w=681&h=383&crop=1"></img>
+        <h2>The death of the void</h2>
+        <h3>21/02/1984</h3>
+        <h4>Fiction</h4>
+        <p>The Archbishop of Canterbury is expected to criticise the government's Illegal Migration Bill as the controversial legislation is debated in the House of Lords today. Justin Welby has previously slated the government's treatment of migrants, including its flagship Rwanda deportation plan.</p>
+      </div>
+      <div className="post">
+        <img src="https://variety.com/wp-content/uploads/2017/04/the-void.jpg?w=681&h=383&crop=1"></img>
+        <h2>The death of the void</h2>
+        <h3>21/02/1984</h3>
+        <h4>Fiction</h4>
+        <p>The Archbishop of Canterbury is expected to criticise the government's Illegal Migration Bill as the controversial legislation is debated in the House of Lords today. Justin Welby has previously slated the government's treatment of migrants, including its flagship Rwanda deportation plan.</p>
+      </div>
+    </main>
   );
 }
 
